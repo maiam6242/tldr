d7a20b9c0b69c1d93ee8435f063b55a2627fb674
maiam6242 <maiam6242@gmail.com>
2018-12-21 20:12:45 -0500

    created and commented getOfficeVersion... need to work on making it work regardless of OS. Need to look more at processes and how they work to do that

--- src/tldr.java
+++ src/tldr.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class tldr {
 
   private static JFrame frame;
@@ -76,12 +79,58 @@ public class tldr {
       mergeFiles();
     }
 
-    checkOfficeVersion();
+    getOfficeVersion();
     //the version of office determines the file type and creation methods
     createExcelFile();
 
   }
 
+  private int getOfficeVersion()
+  {
+    /** Checks what (if any) version of office is on the system.
+     *  Returns 0 if no office is installed and the version number if office is installed
+     *  The version number is the last two digits of the year that the office package came out (83-Present)
+     */
+    //TODO: work on making this work regardless of OS (rn windows dependent) and understanding functionality a little bit better
+    try{
+
+      //creates process which looks for the office version then reads it, WINDOWS DEPENDENT
+
+      Process process = Runtime.getRuntime().exec(new String [] {"cmd.exe", "/c", "assoc", ".xls"} );
+      BufferedReader officeVersionReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+      String officeVersion = officeVersionReader.readLine();
+      officeVersionReader.close();
+
+      //see if any office is installed
+
+      if (officeVersion == null){
+        System.out.println("No Office is Installed");
+        System.exit(1);
+        return 0;
+      }
+
+      //determine what version the office installed is
+      String fileType[] = officeVersion.split("=");
+      process = Runtime.getRuntime().exec(new String [] {"cmd.exe", "/c", "ftype", fileType[1]});
+      officeVersionReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+      String fileAssociation = officeVersionReader.readLine();
+      String fullOfficeVersion = fileAssociation.split("=")[1];
+      officeVersion = fullOfficeVersion.replaceAll("[^0-9]+","");
+      return Integer.parseInt(officeVersion);
+
+    }
+    catch(Exception error){
+        error.printStackTrace();
+    }
+
+
+
+  }
+
+  private void createExcelFile()
+  {
+
+  }
   private void getInputtedKeywords()
   {
 
@@ -121,6 +170,11 @@ public class tldr {
   private void createThreads()
   {
     separateContent();
+
+  }
+
+  private void separateContent()
+  {
 
   }
 

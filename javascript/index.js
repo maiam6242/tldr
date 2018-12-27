ed939082ee1f9301f9b0e5985d1fce6d129db417
maiam6242 <maiam6242@gmail.com>
2018-12-27 12:06:30 -0800

    More fun with websites

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,18 @@
 document.getElementById('date').innerHTML = new Date().toDateString();
+
+function menuDropFunc() {
+  document.getElementById("myDropdown").classList.toggle("show");
+
+}
+window.onclick = function(event) {
+  if(!event.targer.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++){
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
